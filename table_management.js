faa13c8b02d31abfae9bcd1c39246911a408c0bf
Ellie <e3e@disroot.org>
2023-06-20 01:17:04 +0200

    save checked columns

--- table_management.js
+++ table_management.js
@@ -35,9 +35,18 @@ function sortTable() {
 document.addEventListener("DOMContentLoaded", function () {
     const checkboxes = document.querySelectorAll(".hide-column");
     checkboxes.forEach(checkbox => {
+        const column = checkbox.dataset.column;
+        const savedValue = localStorage.getItem("column_" + column);
+        
+        if (savedValue !== null) {
+            checkbox.checked = savedValue === "true";
+        } else {
+            checkbox.checked = false;
+        }
+
         checkbox.addEventListener("change", function () {
-            const column = checkbox.dataset.column,
-                trs = document.querySelectorAll("#firmwareTable tr");
+            localStorage.setItem("column_" + column, checkbox.checked);
+            const trs = document.querySelectorAll("#firmwareTable tr");
             trs.forEach(tr => {
                 let td = tr.children[column];
                 if (td) td.style.display = checkbox.checked ? "" : "none";
