60129a1c4cbf9bc8912bd9ef9473be378431c6d9
Ellie <e3e@disroot.org>
2023-06-20 13:36:36 +0200

    optimise code + minimise html

--- website.py
+++ website.py
@@ -1,12 +1,9 @@
 import yaml
-from urllib.parse import quote
 from pathlib import Path
+from htmlmin import minify as minify_html
 
-def read_file(filename):
-    return Path(filename).read_text()
-
-HTML_PAGE_TEMPLATE = read_file("html_page_template.html")
-FIRMWARE_PAGE_TEMPLATE = read_file("firmware_page_template.html")
+HTML_PAGE_TEMPLATE = Path("html_page_template.html").read_text()
+FIRMWARE_PAGE_TEMPLATE = Path("firmware_page_template.html").read_text()
 
 def generate_404_page():
     body = """
@@ -19,18 +16,22 @@ def generate_404_page():
     return HTML_PAGE_TEMPLATE.format(title="404 - Page Not Found", body=body)
 
 def generate_main_page(models):
-    model_links = "\n".join(f"<a href='{model.replace(' ', '_')}_firmware.html' class='link link-white'>{model}</a>" for model in models)
+    model_links = "\n".join(
+        f"<a href='{model.replace(' ', '_')}_firmware.html' class='link link-white'>{model}</a>"
+        for model in models
+    )
     return HTML_PAGE_TEMPLATE.format(title="Cocaine.Trade", body=build_body(model_links))
 
 def generate_firmware_page(model, firmwares):
-    table_rows = generate_table_rows(firmwares, model)
+    def generate_table_rows():
+        return "\n".join(
+            f"<tr><td><a href='{fw['url']}' class='fw-link'>{fw['Incremental']}</a></td><td>{get_version(fw)}</td><td>{fw['VrShell_Version']}</td><td>{fw['Build_Date']}</td><td>{fw['Fingerprint']}</td></tr>"
+            for fw in firmwares
+        )
+
+    table_rows = generate_table_rows()
     body = FIRMWARE_PAGE_TEMPLATE.format(model=model, table_rows=table_rows)
     return HTML_PAGE_TEMPLATE.format(title=f"{model} Firmware Updates", body=body)
-
-def generate_table_rows(firmwares, model):
-    return "\n".join(
-        f"<tr><td><a href='{fw['url']}' class='fw-link'>{fw['Incremental']}</a></td><td>{get_version(fw)}</td><td>{fw['VrShell_Version']}</td><td>{fw['Build_Date']}</td><td>{fw['Fingerprint']}</td></tr>"
-        for fw in firmwares)
 
 def get_version(fw):
     incremental = int(fw['Incremental'])
@@ -52,9 +53,6 @@ def build_body(model_links):
         <a href='https://blahaj.life' class='link link-white'>Blahaj Life</a>
     """
 
-def build_download_link(model, firmware_name):
-    return f"https://aquavmcorp-my.sharepoint.com/personal/basti_aqualabs_xyz/_layouts/15/download.aspx?SourceUrl=/personal/basti_aqualabs_xyz/Documents/firmware/{quote(model)}/PTC/{firmware_name}.zip"
-
 def generate_site(firmware_file):
     try:
         firmwares = yaml.safe_load(Path(firmware_file).read_text())
@@ -69,9 +67,9 @@ def generate_site(firmware_file):
 
     for model in models:
         model_firmwares = [fw for fw in firmwares if fw['Model'] == model]
-        Path(f"{model.replace(' ', '_')}_firmware.html").write_text(generate_firmware_page(model, model_firmwares))
+        Path(f"{model.replace(' ', '_')}_firmware.html").write_text(minify_html(generate_firmware_page(model, model_firmwares)))
 
-    Path("index.html").write_text(generate_main_page(models))
-    Path("404.html").write_text(generate_404_page())
+    Path("index.html").write_text(minify_html(generate_main_page(models)))
+    Path("404.html").write_text(minify_html(generate_404_page()))
 
 generate_site('firmwares.yaml')
